d6314101a78a353bcf5194c060e0fcb7ec32c0f6
Vladislav Benyaminov <b7v0r7@gmail.com>
2020-06-07 12:12:32 +0300

    Started users app. Added styles.

---
+++ web/static_dev/js/carusel.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("#carousel-header").owlCarousel({
+        items: 1,
+        nav: true,
+        navText: [],
+        loop: true
+    });
+});
--- web/web/urls.py
+++ web/web/urls.py
@@ -1,8 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from web.views import temp_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', temp_view),
+    path('users/', include('users',namespace='users'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
