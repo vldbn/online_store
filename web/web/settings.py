3d0e677a860ea5b9ccbd72fd79938767952f8541
Vladislav Benyaminov <b7v0r7@gmail.com>
2020-06-12 04:17:38 +0300

    Added postgres.

--- web/web/settings.py
+++ web/web/settings.py
@@ -62,13 +62,16 @@ TEMPLATES = [
 
 ASGI_APPLICATION = 'web.asgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+# PostgreSQL
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.environ.get('POSTGRES_PORT')
     }
 }
 
