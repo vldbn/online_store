d5453d05db6aba0bbc45ccc174457cf70ee4f28b
Vladislav Benyaminov <b7v0r7@gmail.com>
2020-06-14 16:18:36 +0300

    Fixes.

--- web/store/views.py
+++ web/store/views.py
@@ -243,6 +243,21 @@ class RecommendationsView(LoginRequiredMixin, View):
 
         return render(request, 'store/product_list.html', context)
 
+    def post(self, request, user_id):
+        user = User.objects.get(id=request.user.id)
+        if 'wish' in request.POST:
+            product_id = request.POST.get('wish')
+            Wish.objects.create(
+                user=user,
+                product_id=product_id
+            )
+        if 'del' in request.POST:
+            product_id = request.POST.get('del')
+            wishes = Wish.objects.filter(product_id=product_id)
+            wish = wishes.filter(user_id=user.id)
+            wish.delete()
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+
 
 class BestsellingView(View):
     """Returns products sorted buy amount of purchases."""
